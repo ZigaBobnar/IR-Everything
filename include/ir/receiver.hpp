ce17c0bae74e62cc94d87d7f3385d293b5f82d6f
Ziga Bobnar <ziga@bobnar.net>
2020-02-10 17:06:57 +0100

    Add basic ir receiver interface

---
+++ include/ir/receiver.hpp
@@ -0,0 +1,17 @@
+#ifndef IR_RECEIVER_HPP_
+#define IR_RECEIVER_HPP_
+
+namespace ir {
+class Receiver {
+private:
+  int receiver_pin_;
+
+public:
+  Receiver(int receiver_pin);
+
+  void receive(int timeout);
+  bool* get_recording();
+};
+}
+
+#endif  // IR_RECEIVER_HPP_
---
+++ src/ir/receiver.cpp
@@ -0,0 +1,15 @@
+#include <ir/receiver.hpp>
+
+namespace ir {
+Receiver::Receiver(int receiver_pin) : receiver_pin_(receiver_pin) {
+
+}
+
+void receive(int timeout) {
+
+}
+
+bool* get_recording() {
+    return nullptr;
+}
+}
