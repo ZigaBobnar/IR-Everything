46d907397a6a58bf0493f04bfe4e275011631a3a
Ziga Bobnar <ziga@bobnar.net>
2020-02-10 17:14:04 +0100

    Basic signal reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
+#define IN_SIGNAL_PIN 0
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(analogRead(IN_SIGNAL_PIN));
+  delay(2);
 }
